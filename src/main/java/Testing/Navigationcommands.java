a8f56881bbce58068eda70bb31b3967695b5fbfe
VinithaKorivi <kvineetha135@gmail.com>
2026-06-04 21:15:51 +0530

    navigation method

---
+++ src/main/java/Testing/Navigationcommands.java
@@ -0,0 +1,27 @@
+package Testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigationcommands {
+
+	public static void main(String[] args) throws InterruptedException {
+		  WebDriver driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		  driver.manage().deleteAllCookies();
+		     driver.get("https://www.google.com/");
+		     Thread.sleep(2000);
+		     
+		     driver.navigate().to("https://www.facebook.com/");
+		     Thread.sleep(2000);
+		     driver.navigate().back();
+		     Thread.sleep(2000);
+		     
+		     driver.navigate().forward();
+		     Thread.sleep(2000);
+		     
+		     driver.navigate().refresh();
+		     Thread.sleep(2000);
+	}
+
+}
