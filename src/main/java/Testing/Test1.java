02ef403e2040449f830670ff8011233c206d2eda
VinithaKorivi <kvineetha135@gmail.com>
2026-06-06 20:43:48 +0530

    test 1

---
+++ src/main/java/Testing/Test1.java
@@ -0,0 +1,29 @@
+package Testing;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+//        driver.get("https://the-internet.herokuapp.com");
+
+//        driver.get("https://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
+        driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
+
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
